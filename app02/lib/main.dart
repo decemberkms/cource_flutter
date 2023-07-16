d3c31d5f33c4c10b52680ecf5d30dc2872fffb26
Min <decemberkms@gmail.com>
2023-07-16 14:36:56 +0200

    Section2 Basics -Changed class name

--- app02/lib/main.dart
+++ app02/lib/main.dart
@@ -3,27 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.deepPurple,
-              Colors.white,
-            ],
-          ),
+      body: GradientContainer(),
+    ),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 103, 58, 183),
+            Color.fromARGB(255, 255, 255, 255),
+          ],
         ),
-        child: const Center(
-          child: Text(
-            "Hello world!",
-            style: TextStyle(
-              fontSize: 28.0,
-              color: Colors.white,
-            ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello world!",
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.white,
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
