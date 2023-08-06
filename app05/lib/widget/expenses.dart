f2e273e2254bef4909f2309d58a1b53393878e14
Min <decemberkms@gmail.com>
2023-08-06 15:35:17 +0200

    Section5 snackbar clear

--- app05/lib/widget/expenses.dart
+++ app05/lib/widget/expenses.dart
@@ -47,6 +47,7 @@ class _Expenses extends State<Expenses> {
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         duration: const Duration(seconds: 3),
         action: SnackBarAction(
