3c374a3318051dc1efcdb7d0a793c3edf4bd531f
Min <decemberkms@gmail.com>
2023-08-07 20:38:22 +0200

    Section5 ThemeData

--- app05/lib/main.dart
+++ app05/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:app05/widget/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
